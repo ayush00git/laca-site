c3ea9b161f90950380a3a624ecbd65ae8970ea81
ayush00git <ayush.2007.iit@gmail.com>
2026-01-02 18:13:19 +0530

    solved the null value error

--- routes/email.js
+++ routes/email.js
@@ -9,6 +9,8 @@ dotenv.config();
 route.get('/verify-email', async (req, res) => {
     const { token } = req.query;
     console.log('You enetred the route gigg');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     try {
         if( !token ) {
             return res.render('verify-email', { 
@@ -16,15 +18,14 @@ route.get('/verify-email', async (req, res) => {
                 message: "Invalid response" 
             });
         };
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const existingUser = await Form.findOne({ email: decoded.email });
         if( existingUser ) {
-            return res.render('verify-email', {
+            return res.status(400).render('verify-email', {
                 success: false,
                 message: `Seat for this email is already reserved`,
             });
         };
-        console.log(`New user: ${decoded.email}`);
+
         const newReg = new Form({
             name: decoded.name,
             email: decoded.email,
@@ -34,10 +35,10 @@ route.get('/verify-email', async (req, res) => {
             branch: decoded.branch,
             subjectCode: decoded.subjectCode,
         });
-
         await newReg.save();
-
-        const avSub = await Subject.findOne({ subjectCode: decoded.subjectCode });
+        console.log(`New user: ${decoded.email}`);
+        
+        const avSub = await Subject.findOne({ code: decoded.subjectCode });
         console.log(`Max seats for this: ${avSub.maxSeats}`);
 
         const sub = await Subject.findOneAndUpdate(
@@ -58,6 +59,7 @@ route.get('/verify-email', async (req, res) => {
             message: `Your seat has been reserved: ${decoded.subjectCode}`,
         });
     } catch(error) {
+        const avSub = await Subject.findOne({ code: decoded.subjectCode });
         await Subject.findOneAndUpdate(
             { code: decoded.subjectCode, seatsFilled: { $lt: avSub.maxSeats } },
             { $inc: { seatsFilled: -1 } },
