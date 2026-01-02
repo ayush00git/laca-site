cfb0baa6b46d848c50c85524ca326e39aa3094bf
ayush00git <ayush.2007.iit@gmail.com>
2026-01-02 18:50:39 +0530

    admin can add subjects from frontend and export to excel enabled

--- routes/subject.js
+++ routes/subject.js
@@ -2,9 +2,15 @@ const express = require("express");
 const route = express.Router();
 const Subject = require("../models/subject");
 
+const subjectCode = ['SA-201', 'SA-202', 'SA-203', 'SA-204', 'SA-205', 'SA-206', 'SA-207', 'SA-208', 'SA-209', 'SA-210', 'SA-211', 'SA-212', 'SA-213'];
+
+route.get('/', (req, res) => {
+    res.render('subject', { subjectCodes: subjectCode });
+});
+
 route.post('/', async (req, res) => {
     const { code, maxSeats } = req.body;
-    if( !code ) return res.status(400).json({ message: "Code is required" });
+    if (!code) return res.status(400).json({ message: "Code is required" });
     const sub = new Subject({
         code,
         maxSeats,
