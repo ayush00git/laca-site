0dbc07046b725e3d615aa0f604a8f95ef669dd17
ayush00git <ayush.2007.iit@gmail.com>
2026-01-02 17:00:49 +0530

    added maxSeats fields

--- routes/subject.js
+++ routes/subject.js
@@ -3,12 +3,14 @@ const route = express.Router();
 const Subject = require("../models/subject");
 
 route.post('/', async (req, res) => {
-    const { code } = req.body;
+    const { code, maxSeats } = req.body;
+    if( !code ) return res.status(400).json({ message: "Code is required" });
     const sub = new Subject({
         code,
+        maxSeats,
     });
     await sub.save();
-    return res.status(200).json({ message: "Subject set" });
+    return res.status(200).json({ message: "Subject added to the database" });
 })
 
 module.exports = route;
